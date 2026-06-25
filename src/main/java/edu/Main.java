f7619999051a8241e0f31b9c78b0eb72af5cbf4b
PBinush <pasidbinush@gmail.com>
2026-06-25 17:42:04 +0530

    update file stucture - 3

--- src/main/java/edu/Main.java
+++ src/main/java/edu/Main.java
@@ -1,7 +1,11 @@
 package edu;
 
+import edu.ui.MainMenu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //program starting entry point
+        MainMenu menu = new MainMenu();
+        menu.start();
     }
 }
